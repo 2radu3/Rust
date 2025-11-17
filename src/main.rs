a60ada5740025d0c70b217a1f68469a1eac44884
2radu3 <2radupmihai3@gmail.com>
2025-11-17 09:44:28 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-// Declare the echo module used to implement echo command
 mod echo;
 mod cat;
 mod head;
@@ -12,23 +11,16 @@ mod mv;
 mod stat;
 
 fn main() {
-    // Retrieve the list of arguments
     let arguments: Vec<String> = std::env::args().collect();
 
-    // Check if a command was specified
     let command = match arguments.get(1) {
-        // Instead of panicking, you can display a help message with the list of all possible
-        // commands.
+
         None => panic!("No command provided"),
         Some(command) => command,
     };
 
-    // Check what command the user specified
     match command.as_str() {
-        // Call the appropiate function. Since echo() function is defined in another module, we have
-        // to use the full path.
-        //
-        // Also, the echo argument takes some arguments. Pass them to the command.
+        
         "echo" => echo::echo(&arguments[2..]),
         "cat" => cat::cat(&arguments[2..]),
         "head" => head::head(&arguments[2..]),
