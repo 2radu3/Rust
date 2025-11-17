aa6ed15a421e5d6c4f6d9a414999f11ac299d550
2radu3 <2radupmihai3@gmail.com>
2025-11-17 09:45:23 +0200

    Update stat.rs

--- src/stat.rs
+++ src/stat.rs
@@ -1,25 +1,57 @@
 use std::fs;
-//use std::os::unix::fs::Metadata;
-//use chrono::DateTime;
+use std::os::unix::fs::MetadataExt; // pentru uid, gid, atime, mtime, ctime
+use chrono::{DateTime, Local};
 
-
-pub fn stat(arguments: &[String]){
-    let file_name = arguments.get(0).unwrap();
-    println!("{}",file_name);
-
-    let meta = fs::metadata(file_name).unwrap();  // follows symlinks
-    // or:
-    // let meta = fs::symlink_metadata("my-symlink")?;
-
-    println!("Size: {} bytes", meta.len());
-    //println!("Type: ");
-    if meta.is_file(){
-        println!("Type: E fisier!!");
-    }
-        else{
-        println!("Type: E director");
+pub fn stat(arguments: &[String]) {
+    let file_name = match arguments.get(0) {
+        Some(name) => name,
+        None => {
+            eprintln!("stat: lipseste numele fisierului");
+            return;
         }
-    
-        
+    };
 
+    let metadata = match fs::symlink_metadata(file_name) {
+        Ok(m) => m,
+        Err(e) => {
+            eprintln!("stat: eroare la citirea '{}': {}", file_name, e);
+            return;
+        }
+    };
+
+    let file_type = if metadata.is_dir() {
+        "Directory"
+    } else if metadata.is_file() {
+        "File"
+    } else if metadata.file_type().is_symlink() {
+        "Symlink"
+    } else {
+        "Other"
+    };
+
+    let permissions = metadata.mode() & 0o777;
+    
+    let uid = metadata.uid();
+    let gid = metadata.gid();
+
+    let size = metadata.len();
+
+    let access_time = DateTime::<Local>::from(
+        std::time::UNIX_EPOCH + std::time::Duration::from_secs(metadata.atime() as u64)
+    );
+    let modify_time = DateTime::<Local>::from(
+        std::time::UNIX_EPOCH + std::time::Duration::from_secs(metadata.mtime() as u64)
+    );
+    let change_time = DateTime::<Local>::from(
+        std::time::UNIX_EPOCH + std::time::Duration::from_secs(metadata.ctime() as u64)
+    );
+
+    println!("File: {}", file_name);
+    println!("Type: {}", file_type);
+    println!("Permissions: {:o}", permissions);
+    println!("Owner: UID={} GID={}", uid, gid);
+    println!("Size: {} bytes", size);
+    println!("Accessed: {}", access_time);
+    println!("Modified: {}", modify_time);
+    println!("Changed: {}", change_time);
 }
